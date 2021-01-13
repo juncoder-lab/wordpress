0780f4eea263622af7e81b415f93edac5ed9d3c5
Sergey S <smirnof.sergey@gmail.com>
2021-01-13 16:08:36 +0300

    13/01/2021

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -1,7 +1,9 @@
 <?php
 add_action('wp_enqueue_scripts', 'childhood_scripts');
-function childhood_scripts () {
+function childhood_scripts ()
+{
     wp_enqueue_style('childhoodStyle', get_stylesheet_uri());
     wp_enqueue_style('childhoodStyle2', get_template_directory_uri() . '/assets/styles/style2.css');
     wp_enqueue_script('childhoodScript', get_template_directory_uri().'/assets/js/main.min.js', array(), null, true);
 }
+    add_theme_support( 'custom-logo' );
